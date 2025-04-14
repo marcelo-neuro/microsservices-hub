88854f499067151bd2a463ebc11e9f06daa952a5
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-14 11:20:38 -0300

    Integraton Test: findAll, findById controller test

---
+++ ms-pagamento/src/test/java/com/github/marcelo_neuro/ms_pagamento/controller/PgamentoControllerIT.java
@@ -0,0 +1,75 @@
+package com.github.marcelo_neuro.ms_pagamento.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.marcelo_neuro.ms_pagamento.dto.PagamentoDTO;
+import com.github.marcelo_neuro.ms_pagamento.tests.Factory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+public class PgamentoControllerIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private Long existingId;
+    private Long nonExistingId;
+    private PagamentoDTO dto;
+
+    @BeforeEach
+    void setup() throws Exception {
+        existingId = 1L;
+        nonExistingId = 50L;
+
+        dto = Factory.createNewPagamentoDTO();
+    }
+
+    @Test
+    public void findAllShouldReturnListAllPagamentos() throws Exception {
+        mockMvc.perform(get("/pagamentos")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("[0].id").value(1L))
+                .andExpect(jsonPath("[0].nome").isString())
+                .andExpect(jsonPath("[0].nome").value("Amadeus Mozart"))
+                .andExpect(jsonPath("[5].status").value("CONFIRMADO"));
+
+    }
+
+    @Test
+    public void findByIdShoudReturnPagamentoDTOWhenIdExists() throws Exception {
+        mockMvc.perform(get("/pagamentos/{id}", existingId)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("id").value(1))
+                .andExpect(jsonPath("nome").isString())
+                .andExpect(jsonPath("nome").value("Amadeus Mozart"))
+                .andExpect(jsonPath("status").value("CRIADO"));
+    }
+
+    @Test
+    public void findByIdSouldThrowResourceNotFoundExceptionWhenIdDoesntExist() throws Exception {
+        mockMvc.perform(get("/pagamentos/{id}", nonExistingId)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+    }
+}
--- ms-pagamento/src/test/java/com/github/marcelo_neuro/ms_pagamento/service/PagamentoServiceIT.java
+++ ms-pagamento/src/test/java/com/github/marcelo_neuro/ms_pagamento/service/PagamentoServiceIT.java
@@ -1,5 +1,6 @@
 package com.github.marcelo_neuro.ms_pagamento.service;
 
+import com.github.marcelo_neuro.ms_pagamento.dto.PagamentoDTO;
 import com.github.marcelo_neuro.ms_pagamento.entity.Pagamento;
 import com.github.marcelo_neuro.ms_pagamento.repository.PagamentoRepository;
 import com.github.marcelo_neuro.ms_pagamento.service.exception.ResourceNotFoundException;
@@ -9,6 +10,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @SpringBootTest
 @Transactional
@@ -40,5 +43,18 @@ public class PagamentoServiceIT {
     @Test
     public void deleteShouldThrowResourceNotFoundEsceptionWhenIdDoesntExist() {
         Assertions.assertThrows(ResourceNotFoundException.class, () -> service.delete(nonExixstingId));
+    }
+
+    @Test
+    public void findAllShouldReturnPagamentoDTOList() {
+
+        List<PagamentoDTO> result = service.getAll();
+
+        Assertions.assertFalse(result.isEmpty());
+        Assertions.assertEquals(countTotPagamentos, result.size());
+        Assertions.assertEquals(Double.valueOf(35.55), result.get(0).getValor().doubleValue());
+        Assertions.assertEquals("Amadeus Mozart", result.get(0).getNome());
+        Assertions.assertEquals("Bartolomeu Pac", result.get(1).getNome());
+        Assertions.assertNull(result.get(5).getNome());
     }
 }

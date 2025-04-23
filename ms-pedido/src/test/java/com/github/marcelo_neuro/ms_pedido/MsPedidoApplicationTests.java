45a438372b1b8ea65d988b3ee66de5c1090dc2da
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-23 09:35:18 -0300

    feat: first commit ms-pedido

---
+++ ms-pedido/src/test/java/com/github/marcelo_neuro/ms_pedido/MsPedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.marcelo_neuro.ms_pedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPedidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

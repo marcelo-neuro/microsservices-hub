ebf91d0a77d270942d96b5181c651133e49a29ed
marcelo-neuro <labsfiap@fiap.com.br>
2025-05-14 09:03:07 -0300

    feat: created api-gateway

--- ms-pedido/src/main/java/com/github/marcelo_neuro/ms_pedido/MsPedidoApplication.java
+++ ms-pedido/src/main/java/com/github/marcelo_neuro/ms_pedido/MsPedidoApplication.java
@@ -1,7 +1,5 @@
 package com.github.marcelo_neuro.ms_pedido;
 
-import com.github.marcelo_neuro.ms_pedido.repository.ItemDoPedidoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;

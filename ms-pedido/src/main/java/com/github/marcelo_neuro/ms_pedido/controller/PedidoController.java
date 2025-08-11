5b5f9a54842a6986da41db9d7085ce08b3ccc0b6
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-11 10:27:21 -0300

    Feat: novos updates de pedido.

--- ms-pedido/src/main/java/com/github/marcelo_neuro/ms_pedido/controller/PedidoController.java
+++ ms-pedido/src/main/java/com/github/marcelo_neuro/ms_pedido/controller/PedidoController.java
@@ -1,8 +1,10 @@
 package com.github.marcelo_neuro.ms_pedido.controller;
 
 import com.github.marcelo_neuro.ms_pedido.dto.PedidoDTO;
+import com.github.marcelo_neuro.ms_pedido.dto.StatusDTO;
 import com.github.marcelo_neuro.ms_pedido.service.PedidoService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -47,6 +49,19 @@ public class PedidoController {
     @PutMapping(path = "/{id}")
     public ResponseEntity<PedidoDTO> update(@PathVariable Long id, @RequestBody @Valid PedidoDTO dto) {
         dto = service.update(id, dto);
+        return ResponseEntity.ok(dto);
+    }
+
+    @PutMapping(path = "/{id}/pago")
+    public ResponseEntity<String> aprovarPagamento(@PathVariable @NotNull Long id) {
+        service.aprovarPagamentoPedido(id);
+        String msg = "Pagamento aprovado, aguarde confirmação do pagamento";
+        return ResponseEntity.ok(msg);
+    }
+
+    @PutMapping(path = "/{id}/status")
+    public ResponseEntity<PedidoDTO> updateStatus(@PathVariable @NotNull Long id, @RequestBody StatusDTO statusDto) {
+        PedidoDTO dto = service.updatePedidoStatus(id, statusDto);
         return ResponseEntity.ok(dto);
     }
 

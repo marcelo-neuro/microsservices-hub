8d8edcd9a19c2ce6cadd931f4b2b2f7215b8dd27
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-11 10:36:04 -0300

    Added Lomook and OpenFeign to ms-pagamento

--- ms-pagamento/src/main/java/com/github/marcelo_neuro/ms_pagamento/dto/StatusDTO.java
+++ ms-pagamento/src/main/java/com/github/marcelo_neuro/ms_pagamento/dto/StatusDTO.java
@@ -1,12 +1,15 @@
 package com.github.marcelo_neuro.ms_pagamento.dto;
 
 import com.github.marcelo_neuro.ms_pagamento.entity.Status;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
 public class StatusDTO {
 
     private Status status;
 
-    public Status getStatus() {
-        return status;
-    }
 }

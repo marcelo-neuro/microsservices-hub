b72a520b03a1757faccfdac7edb0421582ad32d1
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-12 08:18:36 -0300

    Resolved: Spring wasn't injecting PedidoClient

--- ms-pagamento/src/main/java/com/github/marcelo_neuro/ms_pagamento/MsPagamentoApplication.java
+++ ms-pagamento/src/main/java/com/github/marcelo_neuro/ms_pagamento/MsPagamentoApplication.java
@@ -3,9 +3,11 @@ package com.github.marcelo_neuro.ms_pagamento;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class MsPagamentoApplication {
 
 	public static void main(String[] args) {
